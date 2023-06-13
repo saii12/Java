87445c97d515cce58019b0290b1832cfc3580f41
hansan9611 <hansan9611@gmail.com>
2023-06-13 16:44:03 +0900

    Textbook

---
+++ Textbook/src/ch01/sec06/Hello.java
@@ -0,0 +1,15 @@
+package ch01.sec06;
+
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 박한산
+ * 내용 : 교재 p.15 예제
+ * 
+ *
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		
+	}
+}
---
+++ Textbook/src/ch01/sec12/Calculator.java
@@ -0,0 +1,17 @@
+package ch01.sec12;
+
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 박한산 
+ * 내용 : p.30 예제
+ *
+ */
+public class Calculator {
+	public static void main(String[] args) {
+	int x = 1;
+	int y = 2;
+	int result = x + y;
+	System.out.println(result);
+	
+	}
+}
