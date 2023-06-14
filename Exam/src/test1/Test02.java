a4f03c7749e27753f06b8df0b4c41668bbcf8945
hansan9611 <hansan9611@gmail.com>
2023-06-14 10:28:39 +0900

    연습문제

--- Exam/src/test1/Test02.java
+++ Exam/src/test1/Test02.java
@@ -1,5 +1,32 @@
 package test1;
 
+import java.util.Scanner;
+
 public class Test02 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int year;
+		int birth;
+		String name;
+		
+		System.out.println("올해 년도 입력 : ");
+		year = 
+				
+		System.out.println("태어난 년도 입력 : ");
+		birth =
+				
+		System.out.println("이름 입력 : ");
+		name = 
+		
+		int age = year - birth;
+		
+		System.out.println("%s님 안녕하세요.\n당신은 올해 만 %d세 입니다.", name, age);
+		
+		sc.close();
+		
+	}
 
 }
--- Exam/src/test1/Test03.java
+++ Exam/src/test1/Test03.java
@@ -2,4 +2,19 @@ package test1;
 
 public class Test03 {
 
+	public static void main(String[] args) {
+		
+		
+		result = num++;
+		System.out.println("result : " + result);
+		
+		result = ++num;
+		System.out.println("result : " + result);
+		
+		result = num--;
+		System.out.println("result : " + result);
+		
+		result = --num;
+		System.out.println("result : " + result);
+	}
 }
