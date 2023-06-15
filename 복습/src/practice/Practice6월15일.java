4d82df2e1f4ba47c85f834ab601046329d663e00
hansan9611 <hansan9611@gmail.com>
2023-06-15 17:52:47 +0900

    복습

---
+++ 복습/src/practice/Practice6월15일.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Practice6월15일 {
+
+	public static void main(String[] args) {
+		
+		int y1 = f(1);
+		int y2 = f(2);
+		int y3 = f(3);
+		
+		System.out.println("y1 : " + y1);
+		System.out.println("y2 : " + y2);
+		System.out.println("y3 : " + y3);
+		
+		
+	}
+	
+	public static int f(int x) {
+		int y = x * 2 + 3;
+		return y;
+		
+	}
+}
