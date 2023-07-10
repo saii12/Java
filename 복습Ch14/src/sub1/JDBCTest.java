22c3972c8b9f0b66d469543361290d69c1b9273a
hansan9611 <hansan9611@gmail.com>
2023-07-10 13:59:12 +0900

    복습Ch14

---
+++ 복습Ch14/src/sub1/JDBCTest.java
@@ -0,0 +1,32 @@
+package sub1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCTest {
+
+	public static void main(String[] args) {
+		
+		// DB정보
+		String host = "jdbc:mysql://127.0.0.1:3306/userdb";
+		String user = "root";
+		String pass = "1234";
+		
+		try {
+			// JDBC 드라이버 로드
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(host, user, pass);
+			
+			if(conn != null) {
+				System.out.println("데이터베이스 접속 성공!");
+			} else {
+				System.out.println("데이터베이스 접속 실패");
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
